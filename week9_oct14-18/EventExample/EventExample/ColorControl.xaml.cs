1defddc612d34007929b192bb5df9200eaf4b9cd
Julie <juliet@ksu.edu>
2024-10-14 15:21:09 -0500

    after mon oct 14

--- week9_oct14-18/EventExample/EventExample/ColorControl.xaml.cs
+++ week9_oct14-18/EventExample/EventExample/ColorControl.xaml.cs
@@ -20,6 +20,8 @@ namespace EventExample
     /// </summary>
     public partial class ColorControl : UserControl
     {
+        public event EventHandler<ColorEventArgs>? ChangeColor;
+
         public ColorControl()
         {
             InitializeComponent();
@@ -32,8 +34,19 @@ namespace EventExample
 
             //I want to change something in MainWindow
 
-            
+            if (sender is Button b)
+            {
+                if (b.Name == "RedButton")
+                {
+                    ChangeColor?.Invoke(this, new ColorEventArgs("Red"));
+                }
+                else if (b.Name == "BlueButton")
+                {
+                    ChangeColor?.Invoke(this, new ColorEventArgs("Blue"));
+                }
+            }
 
+            
         }
     }
 }
